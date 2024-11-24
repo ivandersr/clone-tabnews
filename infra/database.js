80e7ef041d72d99cb861c2755a8c805d0654f5fa
Ivander <ivandersalvadorruiz@gmail.com>
2024-11-24 14:44:30 -0300

    feat: adiciona `POSTGRES_CA` para certificado com conexão ao digitalocean

--- infra/database.js
+++ infra/database.js
@@ -7,9 +7,7 @@ async function query(queryObj) {
         database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD,
         port: process.env.POSTGRES_PORT,
-        ssl: process.env.NODE_ENV === 'development'
-            ? false
-            : true
+        ssl: getSSLValues(),
     });
 
     console.log('Credenciais do Postgres:', {
@@ -30,6 +28,17 @@ async function query(queryObj) {
     } finally {
         await client.end();
     }
+}
+
+function getSSLValues() {
+    if (process.env.POSTGRES_CA) {
+        return {
+            ca: process.env.POSTGRES_CA
+        };
+    }
+    return process.env.NODE_ENV === 'development'
+        ? false
+        : true;
 }
 
 export default {
