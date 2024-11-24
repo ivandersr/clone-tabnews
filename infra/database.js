5328acb14a59ebe5e393e2503e34d029190dcea6
Ivander <ivandersalvadorruiz@gmail.com>
2024-11-24 14:45:12 -0300

    refactor: remove `console.log` com teste de credenciais

--- infra/database.js
+++ infra/database.js
@@ -10,14 +10,6 @@ async function query(queryObj) {
         ssl: getSSLValues(),
     });
 
-    console.log('Credenciais do Postgres:', {
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        port: process.env.POSTGRES_PORT,
-    });
-
     try {
         await client.connect();
         const result = await client.query(queryObj);
