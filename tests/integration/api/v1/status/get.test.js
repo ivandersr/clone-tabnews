9c9741ce8ab38434c28b135051eae623d0688a55
Ivander <ivandersalvadorruiz@gmail.com>
2024-12-01 13:20:21 -0300

    chore: adicionado `orchestrator.js` a fim de aguardar pelo webserver estar pronto para testes

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,9 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
@@ -11,4 +17,3 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.max_connections).toBe(100);
   expect(responseBody.dependencies.database.active_connections).toBe(1);
 });
-
