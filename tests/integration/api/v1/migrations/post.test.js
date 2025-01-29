f97bf78007d16af1bc75895bc284b76807f11f1e
Ivander Ruiz <ivandersalvadorruiz@gmail.com>
2025-01-29 14:10:25 -0300

    style: applied prettier linting

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,26 +9,30 @@ describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response.status).toBe(201);
-      
+
         const responseBody = await response.json();
-      
+
         expect(Array.isArray(responseBody)).toBe(true);
         expect(responseBody.length).toBeGreaterThan(0);
       });
 
-      test("For the second time", async () => {     
+      test("For the second time", async () => {
         const responseAfterMigrations = await fetch(
           "http://localhost:3000/api/v1/migrations",
           { method: "POST" },
         );
         expect(responseAfterMigrations.status).toBe(200);
-      
-        const responseBodyAfterMigrations = await responseAfterMigrations.json();
-      
+
+        const responseBodyAfterMigrations =
+          await responseAfterMigrations.json();
+
         expect(Array.isArray(responseBodyAfterMigrations)).toBe(true);
         expect(responseBodyAfterMigrations.length).toBe(0);
       });
